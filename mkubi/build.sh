94a01c6358be407e55592c2383571492469ccb24
kevin <zhangjj@bjhuahuan.com>
2015-12-10 09:06:23 +0800

    自动升级完成

--- mkubi/build.sh
+++ mkubi/build.sh
@@ -1,3 +1,3 @@
-mkfs.ubifs -r ./bin -m 2048 -e 126976  -c  6979 -o p1020-ubi.fs 
+mkfs.ubifs -r ./bin -m 2048 -e 126976  -c  6979 -o p2000-ubi.fs 
 
-cp p1020-ubi.fs /tftpboot/p2000-ubi.fs
+cp p2000-ubi.fs /tftpboot/p2000-ubi.fs
