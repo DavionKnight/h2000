8063e16842937cea99e5712b77f9209fc1848a6d
kevin <812964073@qq.com>
2016-06-29 13:49:31 +0800

    save wdt test

---
+++ drivers/kthread/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export KPATH=/home/kevin/works/projects/H20PN-2000/linux-2.6-cloud-2000 
+export K_SRC_PATH=/home/kevin/works/projects/H20PN-2000/linux-2.6-cloud-2000 
+
+export PATH=/opt/ppc/eldk4.2/usr/bin:$PATH
+TARGET_ARCH=powerpc
+
+#export CROSS_COMPILE=ppc_85xxDP-
+
+
+rm ./build -rf
+
+
+make ARCH=powerpc CROSS_COMPILE=ppc_85xxDP- 
+
+cp wdt_test.ko  /tftpboot/
+
+
