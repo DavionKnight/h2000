6100e2efe7110c3c538830d0faad6f92d618d509
kevin <812964073@qq.com>
2016-07-30 11:08:12 +0800

    保存修改syslog的文件系统

--- drivers/iptables-1.4.19.1/build.sh
+++ drivers/iptables-1.4.19.1/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+#  COPYRIGHT NOTICE
+#  Copyright (C) 2016 HuaHuan Electronics Corporation, Inc. All rights reserved
+#
+#  Author       	:Kevin_fzs
+#  File Name        	:/home/kevin/works/projects/H20PN-2000/drivers/iptables-1.4.19.1/build.sh
+#  Create Date        	:2016/07/28 20:26
+#  Last Modified      	:2016/07/28 20:26
+#  Description    	:
+#
+
+#!/bin/bash
 
 export PATH=$PATH:/opt/ppc/eldk4.2/usr/bin:/opt/ppc/eldk4.2/bin
 PREPATH=${PWD}
