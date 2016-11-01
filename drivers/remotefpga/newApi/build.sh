1f48cc7d173981ac8a136b3942670857c71c4095
kevin <812964073@qq.com>
2016-11-01 10:18:55 +0800

    rm old files
    Conflicts:
    
    	drivers/spidrv/spi.c
    	drivers/spidrv/spidpll.c
    	drivers/spidrv/spidrv.c
    	drivers/spidrv/spifpga.c

--- drivers/remotefpga/newApi/build.sh
+++ drivers/remotefpga/newApi/build.sh
@@ -1,6 +1,6 @@
 
 export ARCH=powerpc
-export PATH=/opt/eldk42/usr/bin:/opt/eldk42/bin:$PATH
+export PATH=/opt/ppc/eldk4.2/usr/bin:/opt/ppc/eldk4.2/bin:$PATH
 export CROSS_COMPILE=ppc_85xxDP-
 
 ppc_85xxDP-gcc pxm_drv_fpga.c pxm_drv_fpga_remote.c -o main
