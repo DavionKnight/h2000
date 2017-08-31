6e1f5c1c71c33d366da778ab0ece2cc01e9ee34f
kevin <812964073@qq.com>
2017-08-31 21:03:38 +0800

    add int4 int6

--- drivers/exirq/drv_exirq.c
+++ drivers/exirq/drv_exirq.c
@@ -20,78 +20,126 @@
 #include <linux/interrupt.h>
 #include <linux/irqreturn.h>
 #include <linux/spinlock.h>
-
 #include <asm/io.h>
 #include <asm/irq.h>
 
-volatile unsigned int *pic_vaddr6 = NULL, *pic_vaddr5 = NULL;
-extern void open_softirq(int nr, void (*action)(struct softirq_action *));
+#define GET_MASTER_SLAVE_INT 		0x4	
+#define GET_BOARD_PLUG_INT 			0x6	
+
+struct irq16_share{
+	int id;
+	unsigned int *pic_vaddr;
+	wait_queue_head_t intsem;
+	atomic_t semval;	
+	struct irq16_share *next;
+};
+struct irq16_share *irq16_head = NULL;
+
 extern phys_addr_t get_immrbase(void);
 
-static atomic_t ex_interrupt_has_happen = ATOMIC_INIT(0);
-static wait_queue_head_t ex_interrupt;
+#define MPC85xx_PIC_EIVPR_ADDR(x)	(0x50000 + 0x20*(x))
 
-#define MPC85xx_PIC_EIVPR6		0x500c0
-#define WAIT_FOR_EXTERN_INTERRUPT	0x1
-
-#if 1
-static irqreturn_t exirq_process ( int irq, void *dev_id )
+int add_int_node(struct irq16_share * irq_new)
 {
-	int handled = 0;
-	u32 regdata;
+	struct irq16_share *irq16_tmp;
 
-//	printk("come in irq_process \n");
-#if 1
-	regdata = in_be32(pic_vaddr6);
+	if(NULL == irq_new)
+		return 0;
 
-	//printk("pic_eivpr6 value = %x \n", regdata);
-	if(regdata & 0x40000000)
+	if(NULL == irq16_head)
 	{
-		atomic_set(&ex_interrupt_has_happen, 1);
-		wake_up_interruptible(&ex_interrupt);	
+		irq16_head = irq_new;
+		return 0;
 	}
-#endif
 
-	//spin_lock(&i->lock);
-	handled = 1;
+	irq16_tmp = irq16_head;
 
-	//spin_unlock(&i->lock);
-
-	return IRQ_RETVAL(handled);
+	while(NULL != irq16_tmp->next)
+	{
+		irq16_tmp = irq16_tmp->next;
+	}
+	irq16_tmp->next = irq_new;
+	
+	return 0;
+	
 }
-#endif
+struct irq16_share * get_int_node_by_id(int id)
+{
+	struct irq16_share *irq16_tmp;
+
+	irq16_tmp = irq16_head;
+
+	while(NULL != irq16_tmp)
+	{
+		if(irq16_tmp->id == id)
+			return irq16_tmp;
+		irq16_tmp = irq16_tmp->next;
+	}
+	
+	return NULL;
+	
+}
+int free_int_node()
+{
+	struct irq16_share *irq16_tmp1,*irq16_tmp2;
+
+	irq16_tmp1 = irq16_head;
+
+	while(NULL != irq16_tmp1)
+	{
+		irq16_tmp2 = irq16_tmp1;
+		irq16_tmp1 = irq16_tmp1->next;
+		kfree(irq16_tmp2);
+	}
+	
+	return 0;
+	
+}
+
+static irqreturn_t exirq_process ( int q, void *dev_id )
+{
+	u32 regdata;
+	struct irq16_share *irq = irq16_head;
+	
+	while(NULL != irq)	
+	{
+			regdata = in_be32(irq->pic_vaddr);
+
+			if(regdata & 0x40000000)
+			{
+		printk("get_int  %d\n",irq->id);
+					atomic_set(&irq->semval, 1);
+					wake_up_interruptible(&irq->intsem);	
+			}
+	}
+
+	return IRQ_RETVAL(1);
+}
 
 static long exirq_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-                                                                                       
-        switch (cmd)                                                                      
-        {
-		case WAIT_FOR_EXTERN_INTERRUPT:
-			//printk("wait for extern interrupt\n");
+		struct irq16_share *irq = NULL;
 
-			wait_event_interruptible(ex_interrupt,
-			atomic_read(&ex_interrupt_has_happen)!=0);
-			atomic_set(&ex_interrupt_has_happen, 0);
+		if(cmd > 16)
+				return 0;
 
-			//printk("get an interrupt\n");
-		break;
-		default:
-			printk("cmd=%d error\n",cmd);
-		break;
-	}
-	return 0;
+		irq = get_int_node_by_id(cmd);
+		
+		printk("get_int_node_by_id %d\n",cmd);
+		if(NULL == irq)
+		{
+			printk("arg = %d err\n", cmd);
+			return 0;
+		}	
+		printk("get_int_node_by_id succ, before wait %d\n",cmd);
+
+		wait_event_interruptible(irq->intsem,atomic_read(&irq->semval)!=0);
+		atomic_set(&irq->semval, 0);
+		printk("get_int_node_by_id succ,af wait %d\n",cmd);
+
+		return 0;
 }
-#if 0
-static struct timer_list test_timer;
-static void timer_action(unsigned long aa)
-{
-	atomic_set(&ex_interrupt_has_happen, 1);
-	wake_up_interruptible(&ex_interrupt);	
-	
-	test_timer.expires = jiffies+(HZ*10);
-	add_timer(&test_timer);
-}
-#endif
+
 static struct file_operations exirq_fops = {
          .owner = THIS_MODULE,
          .unlocked_ioctl = exirq_ioctl
@@ -99,41 +147,36 @@ static struct file_operations exirq_fops = {
 
 struct cdev exirq_cdev; 
 int major;
-int exirq_int_init(void)
+int exirq_int_add(int id)
 {
 	unsigned int regdata = 0;
-	int ret = 0;
+	struct irq16_share *int16 = NULL;
 
+	int16 = (struct irq16_share *)kmalloc(sizeof(struct irq16_share),GFP_KERNEL);
+	if(NULL == int16)
+	{
+		printk("exirq_int_add kmalloc err\n");
+		return -1;
+	}
+	int16->id = id;
+	init_waitqueue_head(&int16->intsem);
+	atomic_set(&int16->semval, 0);
+	int16->next = NULL;
 
-	init_waitqueue_head(&ex_interrupt);
-	atomic_set(&ex_interrupt_has_happen, 0);
+	int16->pic_vaddr =(unsigned int *)ioremap(get_immrbase() + MPC85xx_PIC_EIVPR_ADDR(id), 64);
+
+	add_int_node(int16);
+
 #if 1
-	pic_vaddr6 = ioremap(get_immrbase() + MPC85xx_PIC_EIVPR6, 64);
-
 	/* init EIVPR4 to 0x80000 */
 	regdata = 0x80000; /* priority=8; vector=0 */
-//	regdata |= 1<<22;
-	out_be32(pic_vaddr6, regdata);
+	out_be32(int16->pic_vaddr, regdata);
 
 	/* init EIDR4 to let core process the int*/
 	regdata  = 2;
-	out_be32((pic_vaddr6+4), regdata);
+	out_be32((int16->pic_vaddr+4), regdata);
 #endif
-	/* we must use IRQF_SHARED mode. here IRQF_SHARED=0x80 */
-	ret = request_irq(16, exirq_process, 0x80, "IRQ4",1); 
-	if (ret < 0) 
-		printk("irq4 request fail.\n");
-	else 
-		printk("irq4 request success.\n");
-#if 0
-	init_timer(&test_timer);
 
-	test_timer.function = timer_action;
-
-	test_timer.expires = jiffies + (HZ * 10);
-
-	add_timer(&test_timer);
-#endif
 	return 0;
 }
 
@@ -141,42 +184,46 @@ static struct class *exirq_cls;
 dev_t dev_id;
 static int __init exirq_init(void)  
 {  
-  	printk("exirq init...\n");
+		int ret = 0;
+		printk("exirq init...\n");
 
-  	exirq_int_init();
-	
-	alloc_chrdev_region(&dev_id, 0, 1, "exirq");
-	major = MAJOR(dev_id);
+		exirq_int_add(4);
+		exirq_int_add(6);
 
-	cdev_init(&exirq_cdev, &exirq_fops);
-	cdev_add(&exirq_cdev, dev_id, 1);
+		ret = request_irq(16, exirq_process, 0x80, "IRQ16",1); 
+		if (ret < 0) 
+				printk("irq 16 request fail.\n");
+		else 
+				printk("irq 16 request success.\n");
 
-	exirq_cls = class_create(THIS_MODULE, "exirq");
+		alloc_chrdev_region(&dev_id, 0, 1, "exirq");
+		major = MAJOR(dev_id);
 
-	device_create(exirq_cls, NULL, dev_id, NULL, "exirq");	
-	/* for test */
-	/*
-	fpga_int_register_callback(0,test1);
-	fpga_int_register_callback(1,test2);
-	*/
+		cdev_init(&exirq_cdev, &exirq_fops);
+		cdev_add(&exirq_cdev, dev_id, 1);
 
+		exirq_cls = class_create(THIS_MODULE, "exirq");
 
-  	return 0;  
-}  
+		device_create(exirq_cls, NULL, dev_id, NULL, "exirq");	
+
+		return 0;  
+}
 
 static void __exit exirq_exit(void)  
 {
-     	printk(KERN_ALERT "exirq driver exit\n");
+		printk(KERN_ALERT "exirq driver exit\n");
 
-	device_destroy(exirq_cls, MKDEV(major, 0));
-    	class_destroy(exirq_cls);
- 
-	/* 3.3 删除cdev*/
-	cdev_del(&exirq_cdev);
- 
-	/* 3.4 释放设备号*/
-	unregister_chrdev_region(MKDEV(major, 0), 1);
-	free_irq(16,1);
+		free_int_node();
+
+		device_destroy(exirq_cls, MKDEV(major, 0));
+		class_destroy(exirq_cls);
+
+		/* 3.3 删除cdev*/
+		cdev_del(&exirq_cdev);
+
+		/* 3.4 释放设备号*/
+		unregister_chrdev_region(MKDEV(major, 0), 1);
+		free_irq(16,1);
 }  
 
 module_init(exirq_init);  
